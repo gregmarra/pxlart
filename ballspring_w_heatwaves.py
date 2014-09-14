bf37514443e8f52f15fb42a38e8ca478c86863ac
Greg Marra <greg.marra@gmail.com>
2014-09-14 16:42:44 -0700

    Add trailing heat

--- ballspring_w_heatwaves.py
+++ ballspring_w_heatwaves.py
@@ -52,7 +52,35 @@ while True:
 
   fader_filter.stimulate(simulation.project_to_intensity_list(N_LEDS))
 
-  led_colors = [(255*excitement,255*excitement,255*excitement) for excitement in fader_filter.excitements]
+  def proportional_mix_colors(a, b, a_share):
+    return (
+      proportional_mix(a[0], b[0], a_share),
+      proportional_mix(a[1], b[1], a_share),
+      proportional_mix(a[2], b[2], a_share),
+      )
+
+  def proportional_mix(a, b, a_share):
+    return a * a_share + b * (1 - a_share)
+
+  def color_from_excitement(excitement):
+    color0 = pygame.color.THECOLORS["black"]
+    color1 = pygame.color.THECOLORS["red"]
+    color2 = pygame.color.THECOLORS["yellow"]
+    color3 = pygame.color.THECOLORS["white"]
+    if (excitement < 0.25):
+      share = 1 - (excitement / 0.25)
+      return proportional_mix_colors(color0, color1, share)
+    elif (excitement < 0.5):
+      share = 1 - ((excitement - 0.25) / 0.25)
+      return  proportional_mix_colors(color1, color2, share)
+    elif (excitement < 0.75):
+      share = 1 - ((excitement - 0.5) / 0.25)
+      return proportional_mix_colors(color2, color3, share)
+    else:
+      share = 1 - ((excitement - 0.75) / 0.25)
+      return proportional_mix_colors(color3, color3, share)
+
+  led_colors = [color_from_excitement(excitement) for excitement in fader_filter.excitements]
 
   client.put_pixels(led_colors, channel=0)
 
