4128763b483c9ffb6f725c5043253cf40759643c
Greg Marra <greg.marra@gmail.com>
2015-02-06 22:42:09 -0800

    Pastel Rainbow

--- helpers/color_pallette.py
+++ helpers/color_pallette.py
@@ -19,6 +19,25 @@ class ColorPallette(object):
         "FFFFFF"
     ]
 
+    PASTEL_RAINBOW = [
+        "F7977A",
+        "F9AD81",
+        "FDC68A",
+        "FFF79A",
+        "C4DF9B",
+        "A2D39C",
+        "82CA9D",
+        "7BCDC8",
+        "6ECFF6",
+        "7EA7D8",
+        "8493CA",
+        "8882BE",
+        "A187BE",
+        "BC8DBF",
+        "F49AC2",
+        "F6989D"
+    ]
+
     @classmethod
     def colors(self, pallette):
         return [tuple(ord(c) for c in rgbstr.decode('hex')) for rgbstr in pallette]
